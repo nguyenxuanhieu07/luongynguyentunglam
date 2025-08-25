52c9b7da37a854834d5610e800a74600b056c8ec
Raito98 <nguyenxuanhieu071998@gmail.com>
2025-08-25 17:10:32 +0700

    done

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,8 @@ var slide_init={
 		slide_init.list_therapy();
 		slide_init.list_therapy2();
 		slide_init.list_feeling();
+		slide_init.list_trilieu();
+		slide_init.list_sick();
 	},
 	home_banner: function() {
 		var list_slide=$('.section-banner'),
@@ -43,7 +45,7 @@ var slide_init={
 				dots: false,
 				arrow: true,
 				speed: 1000,
-				autoplay: true,
+				autoplay: false,
 				autoplaySpeed: 5000,
 				slidesToShow: 4,
 				slidesToScroll: 4,
@@ -56,7 +58,7 @@ var slide_init={
 							infinite: true,
 							dots: false,
 							centerMode: false,
-							arrows: false,
+							arrows: true,
 						},
 					},
 				],
@@ -84,7 +86,7 @@ var slide_init={
 							infinite: true,
 							dots: false,
 							centerMode: false,
-							arrows: false,
+							arrows: true,
 						},
 					},
 				],
@@ -108,11 +110,11 @@ var slide_init={
 						breakpoint: 575,
 						settings: {
 							slidesToShow: 2,
-							slidesToScroll: 2 ,
+							slidesToScroll: 2,
 							infinite: true,
 							dots: false,
 							centerMode: false,
-							arrows: false,
+							arrows: true,
 						},
 					},
 				],
@@ -136,11 +138,11 @@ var slide_init={
 						breakpoint: 575,
 						settings: {
 							slidesToShow: 2,
-							slidesToScroll: 2 ,
+							slidesToScroll: 2,
 							infinite: true,
 							dots: false,
 							centerMode: false,
-							arrows: false,
+							arrows: true,
 						},
 					},
 				],
@@ -163,13 +165,51 @@ var slide_init={
 					{
 						breakpoint: 575,
 						settings: {
-							slidesToShow: 2,
-							slidesToScroll: 2 ,
+							slidesToShow: 1,
+							slidesToScroll: 1,
 							infinite: true,
 							dots: false,
 							centerMode: false,
-							arrows: false,
+							arrows: true,
 						},
+					},
+				],
+			});
+		}
+	},
+	list_trilieu: function() {
+		var list_slide=$('.list-trilieu'),
+			item=$('.list-trilieu .inner');
+		if(list_slide.length>0&&item.length>1) {
+			list_slide.slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				mobileFirst: true,
+				arrows: true,
+				dots: false,
+				responsive: [
+					{
+						breakpoint: 575,
+						settings: "unslick",
+					},
+				],
+			});
+		}
+	},
+	list_sick: function() {
+		var list_slide=$('.section-sick .list-sick'),
+			item=$('.section-sick .list-sick .inner');
+		if(list_slide.length>0&&item.length>1) {
+			list_slide.slick({
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				mobileFirst: true,
+				arrows: true,
+				dots: false,
+				responsive: [
+					{
+						breakpoint: 575,
+						settings: "unslick",
 					},
 				],
 			});
